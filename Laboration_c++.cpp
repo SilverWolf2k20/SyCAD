b457deaa0281c0478029c7d2a7747df1f875b868
SilverWolf2k20 <74456398+SilverWolf2k20@users.noreply.github.com>
2022-05-28 23:39:09 +0300

    Перенесен класс XxlReader в отдельный файл.
    - Реализовано чтение xml файла версии 1.0
    - Реализована возможность получения данных из xml документа.

--- Laboration_c++.cpp
+++ Laboration_c++.cpp
@@ -1,79 +1,30 @@
-﻿#include <algorithm>
-#include <fstream>
+#include "XmlWriter.h"
+#include <windows.h>
 #include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-
-struct XmlDocument 
-{
-    std::string path;
-    std::string version;
-    std::string encoding;
-};
-
-class XmlReader 
-{
-
-};
 
 //-----------------------------------------------------------------------------
-using std::istreambuf_iterator;
-using std::ifstream;
-using std::string;
-using std::string_view;
-using std::unique_ptr;
-
-using std::make_unique;
-
-//-----------------------------------------------------------------------------
-class XmlWriter 
-{
-public:
-    explicit XmlWriter() 
-        : _buffer{nullptr}
-    {}
-
-    bool load(const string_view& path) noexcept 
-    {
-        ifstream file(path.data());
-
-        if (!file)
-            return false;
-
-        _buffer = make_unique<string>(string((
-            istreambuf_iterator<char>(file)), 
-            istreambuf_iterator<char>()
-        ));
-
-        file.close();
-
-        parse();
-        return true;
-    }
-
-private:
-    bool parse() 
-    {
-
-        for (const auto& symbol: *_buffer) {
-            std::cout << symbol;
-        }
-        return true;
-    }
-
-private:
-    unique_ptr<string> _buffer;
-};
-
 using std::cerr;
+using std::cout;
+using std::endl;
 
 int main()
 {
-    string path = "Data.xml";
+    string path{ "Data.xml" };
 
-    XmlWriter xmlWriter;
+    usl::XmlReader xml;
+    auto xdoc = xml.load(path).value();
 
-    if(!xmlWriter.load(path))
-        cerr << "Can't open input file.\n";
+    // получаем корневой узел
+    auto people = xdoc->getRoot("people");
+    // проходим по всем элементам person
+    if (people != nullopt) {
+        auto persons = people.value().getElements("person");
+        for (const auto& peson : persons) {
+            cout << peson.getAttribute("name").value().value  << '\n';
+            cout << peson.getElement("company").value().value << '\n';
+            cout << peson.getElement("age").value().value     << '\n';
+            cout << endl;
+        }
+    }
+    cout  << "\n\n";
 }
